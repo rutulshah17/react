ff33025e730700144e27c1a4149e5d75ec74009f
Rutul Shah <rutulshah17@gmail.com>
2019-10-29 13:14:31 -0400

    adding onClick handler to toggle button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+
+	constructor() {
+		super();
+		this.state = { displayBio: false };
+		this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+
+	}
+
+	toggleDisplayBio() {
+		this.setState( {displayBio: !this.state.displayBio} );
+	}
+
 	render() {
+		let bio =  this.state.displayBio ? (
+			<div>
+				<p> I live in Toronto and i like to code every day</p>
+				<p> My favourite language is JavaScript, and I think React.js is awesome </p>
+				<p> Besides coding, I also love Pizza and Burritos !</p>
+				<button onClick={this.toggleDisplayBio}> Show Less </button>
+			</div>
+		) : <div> <button onClick={this.toggleDisplayBio}> Read More </button> </div>;
+
 		return (
 			<div>
 				<h1> Hello !</h1>
 				<p> My name is Rutul. I'm a software engineer </p>
 				<p> I am always looking forward to working on meaningful projects.</p>
-				<div>
-					<p> I live in Toronto and i like to code every day</p>
-					<p> My favourite language is JavaScript, and I think React.js is awesome </p>
-					<p> Besides coding, I also love Pizza and Burritos !</p>
-				</div>
+				{bio}
 			</div>
 		)
 	}
